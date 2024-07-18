25939ee0c167dde0489cdaf8c42affb1b176a458
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2024-07-17 22:35:02 -0400

    added fixed

--- final-project/script.js
+++ final-project/script.js
@@ -2,24 +2,11 @@ function idCard() {
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
     var address = document.getElementById('address').value;
-    var age = parseInt(document.getElementById('age').value);
+    var age = document.getElementById('age').value;
     var phoneNumber = document.getElementById('phoneNumber').value;
-    var postFullName = document.getElementById('postFullName').innerHTML;
-    var postAddress = document.getElementById('address').innerHTML;
 
     document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
-
-    const numberArray = [];
-    numberArray.push('age');
-    numberArray.push('phoneNumber');
-
-    for (const i of numberArray) {
-        if (i <= 100) {
-            document.getElementById('age').innerHTML = "Age: " + age;
-        } else {
-            document.getElementById('address').innerHTML = "Phone Nubmer: " + phoneNumber;
-        }
-    }
-};
-
-
+    document.getElementById('displayAge').innerHTML = "Age: " + age;
+    document.getElementById('displayPhoneNumber').innerHTML = "Phone Number: " + phoneNumber;
+    document.getElementById('displayAddress').innerHTML = "Address: " + address;
+}
